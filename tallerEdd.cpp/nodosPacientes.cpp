b1e1d9071b499ebfdd84574f2a091a250224a3bf
Andres Cifuentes <andressetneufic@gmail.com>
2024-03-09 21:56:56 -0500

    cuarta subida

--- tallerEdd.cpp/nodosPacientes.cpp
+++ tallerEdd.cpp/nodosPacientes.cpp
@@ -4,9 +4,27 @@
 
 using namespace std;
 
+void aggNodo(){
+
+}
+
+void verNodo(){
+
+}
+
 int main(){
 
-    
+    int op=0;
 
+    do
+    {
+        cout<<"\n\tBIENVENIDO, SENIOR, SENIORA O SENIORE\n"<<endl;
+        cout<<"1. AGREGAR UN PACIENTE"<<endl;        
+        cout<<"2. VER PACIENTES AGREGADOS"<<endl;
+        cout<<"3. SALIR DEL HOSPITALITO\n"<<endl;
+        cin>>op; system("cls");
+    } while (op!=3);
+    
+    system("pause");
     return 0;
 }
