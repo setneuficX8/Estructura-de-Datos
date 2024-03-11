ce88bfa9af1a5383e20bda1b4c69454c2a68bde9
Andres Cifuentes <andressetneufic@gmail.com>
2024-03-10 21:12:43 -0500

    sextaPrueba

--- tallerEdd.cpp/nodosPacientes.cpp
+++ tallerEdd.cpp/nodosPacientes.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
-#include <stdlib.h>
 #include <malloc.h>
 
 using namespace std;
 
-void aggNodo(){
+struct pacientes
+{
+    char nombre=' ';
+    int doc=0;
+    pacientes *sig;
+}; pacientes *cabeza, *aux, *aux2;
 
+
+void aggPaciente(){
+    if(cabeza==NULL){
+            cabeza= (struct pacientes *) malloc (sizeof(struct pacientes));
+
+            cout<<"\nINGRESE SU NOMBRE: "<<endl; cin>>cabeza->nombre;
+            cout<<"\nINGRESE SU NUMERO DE DOCUMENTO: "<<endl; cin>>cabeza->doc;
+        cabeza->sig= NULL;
+    } else{
+        aux= (struct pacientes *) malloc (sizeof(struct pacientes));
+            cout<<"\nINGRESE SU NOMBRE: "<<endl; cin>>aux->nombre;
+            cout<<"\nINGRESE SU NOMBRE: "<<endl; cin>>aux->doc;
+        aux->sig= NULL; aux2= cabeza;
+
+        while (aux2->sig!=NULL)
+        {
+            aux2= aux2->sig;
+            aux2->sig= aux;
+            aux2= aux= NULL;
+
+            free(aux);
+            free(aux2);
+        }
+        
+    }
 }
 
-void verNodo(){
+void verPaciente(){
     
 }
 
@@ -23,8 +52,20 @@ int main(){
         cout<<"2. VER PACIENTES AGREGADOS"<<endl;
         cout<<"3. SALIR DEL HOSPITALITO\n"<<endl;
         cin>>op; system("cls");
+
+        switch (op)
+        {
+        case 1: aggPaciente();
+            break;
+        case 2: verPaciente();
+            break;
+
+        case 3: cout<<"FIN DE LA JORNADA :)\n"<<endl;
+            break;
+        default: cout<<"\nLA OPCION ELEGIDA NO EXISTE, ELIJA UNA DISPONIBLE\n"<<endl;
+            break;
+        }
     } while (op!=3);
     
-    system("pause");
     return 0;
 }
