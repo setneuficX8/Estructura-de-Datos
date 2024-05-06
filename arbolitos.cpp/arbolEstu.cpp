008422ddb1db1bba7d6db8461d9577062d0d6ccc
Andres Cifuentes <andressetneufic@gmail.com>
2024-05-06 16:33:15 -0500

    v2arbols

--- arbolitos.cpp/arbolEstu.cpp
+++ arbolitos.cpp/arbolEstu.cpp
@@ -16,93 +16,96 @@ struct estudiante
     estudiante *izq, *dere;    
 }; estudiante *raiz, *aux, *aux2;
 
-void posicionarMCodigo(){
+void posXCodigo(){
     if(aux->codigo < aux2->codigo){
         if(aux2->izq!=NULL){
             aux2= aux2->izq;
-            posicionarMCodigo();
+            posXCodigo();
         } else {
             aux2->izq= aux;
-        } else if(aux->codigo > aux2->val){
-            if(aux2->der!= NULL){
-                aux2 = aux2->der;
-                posicionarMCodigo();
-            } else {
-                aux2->der = aux;
-            }
         }
+    }   else if(aux->codigo > aux2->codigo){
+            if(aux2->dere!=NULL){
+                aux2= aux2->dere;
+                posXCodigo();
+            } else{
+                aux2->dere= aux;
+            }
     }
 }
 
-void recorrido (){
-    
+void posXFecha(){
+    if(aux->anio < aux2->anio || (aux->anio == aux2->anio && aux->mes < aux2->mes) || (aux->anio == aux2->anio && aux->mes == aux2->mes && aux->dia < aux2->dia)){
+        if(aux2->izq!=NULL){
+            aux2= aux2->izq;
+            posXFecha();
+        } else {
+            aux2->izq= aux;
+        } 
+    } else if(aux->anio < aux2->anio || (aux->anio == aux2->anio && aux->mes < aux2->mes) || (aux->anio == aux2->anio && aux->mes == aux2->mes && aux->dia < aux2->dia)){
+        if(aux2->dere!= NULL){
+                aux2 = aux2->dere;
+                posXFecha();
+            } else {
+                aux2->dere = aux;
+         }
+    }   
 }
 
-void agregar(){
-    aux= (struct estudiante*) malloc (sizeof(estudiante));
+void agregar(){int opc;
+    do {
+        aux= (struct estudiante*) malloc (sizeof(struct estudiante));
 
-    cout<<"NOMBRE: "; cin>>aux->nombre;
-    cout<<"APELLIDO: ", cin>>aux->apellido;
-    cout<<"CODIGO: "; cin>>aux->codigo;
-    cout<<"DIA DE NACIMIENTO: "; cin>>aux->dia;
-    cout<<"MES DE NACIMIENTO: ", cin>>aux->mes;
-    cout<<"ANIO DE NACIMIENTO: "; cin>>aux->anio;
+        cout<<"NOMBRE: "; cin>>aux->nombre;
+        cout<<"APELLIDO: ", cin>>aux->apellido;
+        cout<<"CODIGO: "; cin>>aux->codigo;
+        cout<<"DIA DE NACIMIENTO: "; cin>>aux->dia;
+        cout<<"MES DE NACIMIENTO: ", cin>>aux->mes;
+        cout<<"ANIO DE NACIMIENTO: "; cin>>aux->anio;
 
-    aux->izq= aux->dere = NULL;
-    if(raiz==NULL){
-        raiz= aux;
-        aux= NULL;
-        free(aux); 
-    }else {
-        posicionarMCodigo();
-    }
+        aux->izq= aux->dere = NULL;
+        if(raiz==NULL){
+            raiz= aux;
+            aux= NULL;
+           free(aux); 
+        }else {
+            aux2= raiz;
+            posXCodigo(); //posXFecha();
+        }
         cout<<"REGISTRO REALIZADO"<<endl;
+        
+        cout<<"\nQUIERES HACER OTRO REGISTRO?: (1.SI/2.NO)\n"; cin>>opc; cout<<"\n"<<endl;
+    } while (opc!=2);
+    
+    return;
 }
 
 void preorden(estudiante *recur){
-    if(recur==NULL){
-        return;
-    } else {
-        cout<<"CODIGO: "<<recur->codigo<<endl;
-        cout<<"DIA: "<<recur->dia<<" |MES: "<<recur->mes<<" |ANIO: "<<recur->anio<<endl;
-        cout<<"----"<<endl;
-        preorden(recur->izq); preorden(recur->dere);
-    }
+        cout<<recur->codigo<<endl;cout<<"----"<<endl;
+        if(recur->izq!=NULL){
+            preorden(recur->izq);
+        } if(recur->dere!=NULL){
+            preorden(recur->dere);
+        }
 }
 
 void inorden(estudiante *recur){
-    if(recur==NULL){
-        return;
-    } else {
-        inorden(recur->izq);
-        cout<<"CODIGO: "<<recur->codigo<<endl;
-        cout<<"DIA: "<<recur->dia<<" |MES: "<<recur->mes<<" |ANIO: "<<recur->anio<<endl;
-        cout<<"----"<<endl;
-        inorden(recur->dere);
-    }
+        if(recur->izq!=NULL){
+            inorden(recur->izq);
+        }
+            cout<<recur->codigo<<endl; cout<<"------"<<endl;
+        if(recur->dere!=NULL){
+            inorden(recur->dere);
+        }
 }
 
 void postorden(estudiante *recur){
-    if(recur==NULL){
-        return;
-    } else {
-        postorden(recur->izq); postorden(recur->dere);
-        cout<<"CODIGO: "<<recur->codigo<<endl;
-        cout<<"DIA: "<<recur->dia<<" |MES: "<<recur->mes<<" |ANIO: "<<recur->anio<<endl;
-        cout<<"-----"<<endl;
-    }
-}
-
-void mostrar(estudiante *recur, int conta){
-    if(recur==NULL){
-        return;
-    } else {
-        mostrar(recur->izq, conta+1);
-        for(int i=0; i<conta; i++){
-            cout<<"     ";
-        } cout<<recur->codigo<<endl;
-        mostrar(recur->dere, conta+1);
-    }
+        if(recur->izq!=NULL){
+            postorden(recur->izq);
+        } if(recur->dere!=NULL){
+            postorden(recur->dere);
+        }
+            cout<<recur->codigo<<endl;cout<<"------"<<endl;
 }
 
 void eliminar(){
@@ -117,14 +120,13 @@ int main(){
     {
         cout<<"\nWELCOME TO THE CENTRO ADMINISTRATIVO DE ESTUDIANTES\n"<<endl;
         cout<<"1. REGISTRAR"<<endl;
-        cout<<"2. POSICIONAR MEDIANTE PRE-ORDEN"<<endl;
-        cout<<"3. POSICIONAR MEDIANTE IN-ORDEN"<<endl;
-        cout<<"4. POSICIONAR MEDIANTE POST-ORDEN"<<endl;
-        cout<<"5. MOSTRAR EL ARBOL BINARIO"<<endl;
-        cout<<"6. ELIMINAR UN ESTUDIANTE DEL SISTEMA"<<endl;
-        cout<<"7. SALIR"<<endl;
+        cout<<"2. USAR RECORRIDO PRE-ORDEN"<<endl;
+        cout<<"3. USAR RECORRIDO IN-ORDEN"<<endl;
+        cout<<"4. USAR RECORRIDO POST-ORDEN"<<endl;
+        cout<<"5. ELIMINAR UN ESTUDIANTE DEL SISTEMA"<<endl;
+        cout<<"6. SALIR"<<endl;
 
-        cout<<"ELIGE: "<<endl; cin>>op; system("cls");
+        cout<<"\nELIGE: "<<endl; cin>>op; system("cls");
 
         switch (op)
         {
@@ -133,11 +135,26 @@ int main(){
             case 3: inorden(raiz); break;
             case 4: postorden(raiz); break;
             case 5: eliminar(); break;
-            case 6: mostrar(raiz, 0); break;
-            case 7: cout<<"GRACIAS POR USAR EL SERVICIO"<<endl; break;
+            case 6: cout<<"GRACIAS POR USAR EL SERVICIO"<<endl; break;
             default: cout<<"ESA OPCION NO EXISTE, ELIJA LO DEL MENU"<<endl; break;
         }
-    } while (op!=7); system ("pause");
+    } while (op!=6); system ("pause");
 
     return 0;
 }
+
+/*void recorrido (){
+    aux= raiz;
+    if(raiz!=NULL){
+        preorden(raiz);
+    }
+
+    if(raiz!=NULL){
+        inorden(raiz);
+    }
+
+    if(raiz!=NULL){
+        postorden(raiz);
+    }
+}
+*/
